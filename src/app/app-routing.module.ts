3766a11929d45379a79e50ef1df5ab879c4a7808
Witer <witer_m19@hotmail.com>
2021-01-21 19:26:06 -0300

    Implementando home, contato, sobre e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //rota home
+  { path: 'home', component: HomeComponent },
+  //rota contato
+  { path: 'contato', component: ContatoComponent },
+  //rota sobre
+  { path: 'sobre', component: SobreComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
