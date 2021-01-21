012a0663f4abd40efe4aaf93bfc070782903a7aa
Luccas Yuri dos Santos <luccas.yui@hotmail.com>
2021-01-21 18:47:46 -0300

    Implementando Front RaizAgro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContatoComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
