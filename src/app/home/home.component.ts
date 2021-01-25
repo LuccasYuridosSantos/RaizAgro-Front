953383d5f927436461c2d145210150eb8024dc37
ThiagoBarassa <68078882+ThiagoBarassa@users.noreply.github.com>
2021-01-24 22:53:27 -0300

    funcionalidade mostrar ou ocultar senha ao entrar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,19 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+
+    var btn = (<HTMLSelectElement>document.querySelector('#btn-senha'));
+    var input =(<HTMLInputElement>document.querySelector('#senha'));
+    btn.addEventListener('click', function() {
+      if(input.getAttribute('type') == 'password') {
+          input.setAttribute('type', 'text');
+          btn.innerHTML = '<i class="fa fa-eye-slash" aria-hidden="true"></i>';
+      } else {
+          input.setAttribute('type', 'password');
+          btn.innerHTML = '<i class="fa fa-eye" aria-hidden="true"></i>';
+      }
+    });
   }
 
 }
