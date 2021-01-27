9f37996f92ba6f3c05e7519a43e0c78ebbc201a7
ThiagoBarassa <68078882+ThiagoBarassa@users.noreply.github.com>
2021-01-26 21:17:35 -0300

    Correção bug

--- src/app/model/Usuario.ts
+++ src/app/model/Usuario.ts
@@ -1,17 +1,12 @@
-import { Postagem } from "./Postagem"
+import { Postagem } from "./postagem";
 
-export class Usuario {
-
-    public id: number
-    public nomeCompleto: string
-    public usuario: string
-    public email: string
-    public senha: string
-    public tipo: string
-    public foto: string
-
+export class usuario{
+    public id: number;
+    public nomeCompleto: string;
+    public usuario: string;
+    public email: string;
+    public senha: string;
+    public foto: string;
+    public tipo: string;
     public postagem: Postagem[]
-
-
-
 }
