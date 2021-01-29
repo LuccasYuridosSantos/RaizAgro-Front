b59ae5eb4c6634a938f999ef77ae15e0665d2391
ThiagoBarassa <68078882+ThiagoBarassa@users.noreply.github.com>
2021-01-28 21:58:27 -0300

    att

--- src/app/model/postagem.ts
+++ src/app/model/postagem.ts
@@ -2,13 +2,12 @@ import { Tema } from "./tema"
 import { usuario } from "./usuario"
 
 export class Postagem{
-    
     public id: number
     public descricao: string
     public localizacao: string
     public foto: string
     public dataPostagem: Date
-    public contato: string
+    public maduro: boolean
     public tema: Tema
     public usuario: usuario
 }
