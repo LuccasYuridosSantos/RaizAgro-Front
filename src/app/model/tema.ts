6d8ff60ba3f666be646117e64e235a3c15a87cae
ThiagoBarassa <68078882+ThiagoBarassa@users.noreply.github.com>
2021-01-28 21:57:53 -0300

    att

--- src/app/model/tema.ts
+++ src/app/model/tema.ts
@@ -1,11 +1,8 @@
 import { Postagem } from "./postagem"
 
 export class Tema{
-
     public id: number
-    public nome: string
     public descricao: string
-    public ativo: boolean
     public postagem: Postagem[]
-    
+
 }
