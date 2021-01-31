075028000027594f69c85fcaa1856e290eceb272
Witer <witer_m19@hotmail.com>
2021-01-31 13:43:02 -0300

    Adicionando componente Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { TemaComponent } from './tema/tema.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component'
     TemaComponent,
     TemaEditComponent,
     TemaDeleteComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
